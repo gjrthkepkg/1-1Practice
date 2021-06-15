31d4db3e74fe6f488690656e0b7520b3c1dbd28a
澤淵 徐 <vi49th161224@gmail.com>
2021-06-15 15:27:09 +0800

    2-1

--- 1-1Practice/Default.aspx.cs
+++ 1-1Practice/Default.aspx.cs
@@ -9,6 +9,9 @@ namespace _1_1Practice {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
             mt_GetString();
+            float fa = 3.1415926f;
+            fa = (float)Math.Floor(fa * 1000) / 1000;
+            Response.Write("<br />" + fa);
         }
     }
 }
